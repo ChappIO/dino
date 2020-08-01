ee5b744147886c21dfca6aad8a75e795ff11467a
Thomas Biesaart <thomas.biesaart@gmail.com>
2020-08-01 14:26:01 +0200

    Updates

---
+++ precache-manifest.b31faaa48b58aba50d9720f9a2ca1131.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a2728c94cf5e238abdddc8d68e92ebf",
+    "url": "/dino/index.html"
+  },
+  {
+    "revision": "104db013589061089124",
+    "url": "/dino/static/css/main.f2f61d0f.chunk.css"
+  },
+  {
+    "revision": "6cf7da35f687a137fc06",
+    "url": "/dino/static/js/2.da86b336.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dino/static/js/2.da86b336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "104db013589061089124",
+    "url": "/dino/static/js/main.d7b3a4b3.chunk.js"
+  },
+  {
+    "revision": "398791f787ce605ca57d",
+    "url": "/dino/static/js/runtime-main.4d8a71fb.js"
+  }
+]);
